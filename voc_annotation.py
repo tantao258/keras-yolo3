ee5473ef689a2d2d8dd3b9b30eccf739107100ea
tantao258 <823232162@qq.com>
2018-07-06 15:14:20 +0800

    updata

--- voc_annotation.py
+++ voc_annotation.py
@@ -14,7 +14,7 @@ def convert_annotation(year, image_id, list_file):
     for obj in root.iter('object'):
         difficult = obj.find('difficult').text
         cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
+        if cls not in classes or int(difficult) == 1:
             continue
         cls_id = classes.index(cls)
         xmlbox = obj.find('bndbox')
